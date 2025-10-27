c6e44c352bab5a18fc81a6f8cb159d6c94f18908
calcium-jpg <calvin@calcium.place>
2025-10-26 23:33:25 -0500

    forgot to remove build folder

--- build/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/home/user/steam/steamcmd.sh +force_install_dir /home/user/satisfactory +login anonymous +app_update 1690800 validate +quit
-
-exec ./Engine/Binaries/Linux/FactoryServer-Linux-Shipping FactoryGame
